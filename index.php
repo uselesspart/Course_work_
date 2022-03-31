ca60f7684ea6f5858a7e99f0a760d5824ecb5b32
uselesspart <uselesspartinfo@gmail.com>
2022-03-31 18:42:41 +0300

    git push

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
     <head>
-       <title> Referrer-Page </title>
+       <title> Информация о запросах </title>
     </head>
     <body>
-        <a href="a.php"> Data-collector</a>
+    <form action = "a.php" method = "post">
+        Дата начала <br><input type="text" name="begin_date"><br>
+        Дата конца <br><input type="text" name="end_date"><br>
+        <input type="submit" value = "Отправить">
+        </form>
     </body>
 </html>
