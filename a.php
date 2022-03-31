f5d26db8d478a8076685baeacde05894348f0736
uselesspart <uselesspartinfo@gmail.com>
2022-03-31 14:52:40 +0300

    git push

--- a.php
+++ a.php
@@ -4,15 +4,17 @@
     $host = gethostname();
     $page = getenv("HTTP_REFERER");
     $link = mysqli_connect("localhost", "root", "","data");
+    $time = (string)date("H:i:s d M Y");
     if($link == false){
-        print("Ошибка подключения!" .mysqli_connect_error());
+        print("Ошибка подключения!  " .mysqli_connect_error());
     }
-    else print ("Соединение установлено!");
-    $sql = 'INSERT INTO info SET ip =.$ip.';
+    else print ("Соединение установлено!  ");
+    $sql = "INSERT INTO info SET ip = '.$ip.',  system ='.$system.', host='.$host.', page = '.$page.', time = '.$time.'";
     $result = mysqli_query($link, $sql);
     if($result == false){
-        print("Ошибка запроса!");
+        print("Ошибка запроса!  ");
     }
+    else print ("Запрос успешен!  ");
     echo '<br><br>';
     echo getenv("REMOTE_ADDR");
     echo '<br><br>';
