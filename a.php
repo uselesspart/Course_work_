0bd72a18832810fe76f59f1ab3826e3cae9ceca8
MrP4r7 <ivanfranskevich104@gmail.com>
2022-03-31 19:24:08 +0300

    git push

--- a.php
+++ a.php
@@ -1,20 +1,26 @@
 <?php
+    //Объявление переменных
     $system = php_uname();
     $ip = getenv("REMOTE_ADDR");
     $host = gethostname();
     $page = getenv("HTTP_REFERER");
     $link = mysqli_connect("localhost", "root", "","data");
     $time = (string)date("H:i:s d M Y");
+    //Подключение к базе для записи данных пользователя
+    //Проверка подключения(потом удалить)
     if($link == false){
         print("Ошибка подключения!  " .mysqli_connect_error());
     }
     else print ("Соединение установлено!  ");
+    //Запись данных пользователя в таблицу
     $sql = "INSERT INTO info SET ip = '.$ip.',  system ='.$system.', host='.$host.', page = '.$page.', time = '.$time.'";
     $result = mysqli_query($link, $sql);
+    //Проверка успешности запроса(потом удалить)
     if($result == false){
         print("Ошибка запроса!  ");
     }
     else print ("Запрос успешен!  ");
+    //Вывод сырых данных на экран(потом удалить)
     echo '<br><br>';
     echo getenv("REMOTE_ADDR");
     echo '<br><br>';
@@ -27,4 +33,28 @@
     echo getenv("HTTP_REFERER");
     echo '<br><br>';
     echo date("H:i:s d M Y");
+    //Запросы на получение данных
+    $sql = 'SELECT id, ip, system, host, page, time FROM info';
+    $result = mysqli_query($link, $sql);
+    $db = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $table = array(array());
+    $n = $db[count($db)-1]['id'] - $db[0]['id'];
+    for($i = 0; $i < $n; $i++){
+        $table[$i][1] = $db[$i]['ip'];
+        $table[$i][2] = $db[$i]['system'];
+        $table[$i][3] = $db[$i]['host'];
+        $table[$i][4] = $db[$i]['page'];
+        $table[$i][5] = $db[$i]['time'];
+    }
+    //Отрисовка таблицы
+    echo '<table border = "1"';
+    echo '<tr><td>IP</td><td>System</td><td>Host</td><td>Page</td><td>Time</td>';
+    for($i = 0; $i < $n; $i++){
+        echo '<tr>';
+        for($j = 1; $j <= 5; $j++){
+            echo "<td>  ".$table[$i][$j]." </td>";
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
 ?>
