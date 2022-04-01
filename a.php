2c6d8cc6d7af5f9b909a98e52db01f0b4aa52f60
uselesspart <uselesspartinfo@gmail.com>
2022-04-01 22:42:01 +0300

    git push

--- a.php
+++ a.php
@@ -1,30 +1,45 @@
 <?php
+    //Функция перевода даты
+
+    //Объявление переменных
+    $start_date=date_format(date_create($_POST['calendar1']), "H:i:s d M Y");
+    $end_date = date_format(date_create($_POST['calendar2']), "H:i:s d M Y");
     $system = php_uname();
     $ip = getenv("REMOTE_ADDR");
     $host = gethostname();
     $page = getenv("HTTP_REFERER");
+    $time = date("H:i:s d M Y");
     $link = mysqli_connect("localhost", "root", "","data");
-    $time = (string)date("H:i:s d M Y");
-    if($link == false){
-        print("Ошибка подключения!  " .mysqli_connect_error());
-    }
-    else print ("Соединение установлено!  ");
+    //Подключение к базе для записи данных пользователя
+    //Запись данных пользователя в таблицу
     $sql = "INSERT INTO info SET ip = '.$ip.',  system ='.$system.', host='.$host.', page = '.$page.', time = '.$time.'";
     $result = mysqli_query($link, $sql);
-    if($result == false){
-        print("Ошибка запроса!  ");
+    //Запросы на получение данных
+    $sql = "SELECT id, ip, system, host, page, time FROM info WHERE(time > '.$start_date.' AND time < '.$end_date.')";
+    $result = mysqli_query($link, $sql);
+    $db = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $table = array(array());
+    if(count($db) <= 0) print '<br>Не найдено запросов в выбранный временной промежуток<br>';
+    else
+    {
+    $n = $db[count($db)-1]['id'] - $db[0]['id'];
+    for($i = 0; $i < $n; $i++){
+        $table[$i][1] = $db[$i]['ip'];
+        $table[$i][2] = $db[$i]['system'];
+        $table[$i][3] = $db[$i]['host'];
+        $table[$i][4] = $db[$i]['page'];
+        $table[$i][5] = $db[$i]['time'];
     }
-    else print ("Запрос успешен!  ");
-    echo '<br><br>';
-    echo getenv("REMOTE_ADDR");
-    echo '<br><br>';
-    echo '<br><br>';
-    echo  '<br><br>';
-    echo php_uname();
-    echo  '<br><br>';
-    echo gethostname();
-    echo '<br><br>';
-    echo getenv("HTTP_REFERER");
-    echo '<br><br>';
-    echo date("H:i:s d M Y");
+    //Отрисовка таблицы
+    echo '<table border = "1"';
+    echo '<tr><td>IP</td><td>System</td><td>Host</td><td>Page</td><td>Time</td>';
+    for($i = 0; $i < $n; $i++){
+        echo '<tr>';
+        for($j = 1; $j <= 5; $j++){
+            echo "<td>  ".$table[$i][$j]." </td>";
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    }
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
-    <head>
-       <title> Информация о запросах </title>
-    </head>
-    <body>
-    <form action = "a.php" method = "post">
-        Дата начала <br><input type="text" name="begin_date"><br>
-        Дата конца <br><input type="text" name="end_date"><br>
-        <input type="submit" value = "Отправить">
-        </form>
-    </body>
+<head>
+<title> Информация о запросах </title>
+</head>
+<body>
+<form action = "a.php" method = "post">
+Дата начала <br><input type="datetime-local" name="calendar1" value="2022-03-31" max="2022-04-30" min="2022-03-31"><br>
+Дата конца <br><input type="datetime-local" name="calendar2" value="2022-03-31" max="2022-04-30" min="2022-03-31"><br>
+<input type="submit" value = "Отправить">
+</form>
+</body>
 </html>
