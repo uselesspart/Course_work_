9c38cf859c1669bd2d1f7aa4ac8c35fee4b98e72
uselesspart <uselesspartinfo@gmail.com>
2022-05-31 20:08:34 +0300

    gbgkg

--- a.php
+++ a.php
@@ -62,14 +62,13 @@
     else $os = "Неизвестный";
     function get_browser_name($user_agent)
     {
-    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
-    elseif (strpos($user_agent, 'Edg')) return 'Edge';
-    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
-    elseif (strpos($user_agent, 'Safari')) return 'Safari';
-    elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
-    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
-   
-    return 'Other';
+    if (strpos($user_agent, "Opera") || strpos($user_agent, "OPR/")) return "Opera";
+    elseif (strpos($user_agent, "Edg")) return "Edge";
+    elseif (strpos($user_agent, "Chrome")) return "Chrome";
+    elseif (strpos($user_agent, "Safari")) return "Safari";
+    elseif (strpos($user_agent, "Firefox")) return "Firefox";
+    elseif (strpos($user_agent, "MSIE") || strpos($user_agent, "Trident/7")) return "Internet Explorer";
+    return "Неизвестный";
     }   
     $browser = get_browser_name($user_agent);
     $system = $browser . " " . $os;
